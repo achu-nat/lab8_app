eef95373e84a3b9fe30e6150675a0a6721acc991
spodduturu <spodduturu@ucmerced.edu>
2025-11-16 22:23:37 -0800

    just changed the comment

--- app.py
+++ app.py
@@ -148,7 +148,7 @@ if __name__ == "__main__":
 # 1. pip install -r requirements.txt (this installs all necessary flask packages)
 # 2. run python seed.py
 # 3. run python app.py
-
+#eeeee
 # these are the logins that work:
 # admin (username)/ admin (password)
 # mindy (username) / 123 (password)
