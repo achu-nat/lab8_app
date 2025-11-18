dc3bde54edb62e69a042ecf7d168cb6cc0a9d723
brydotcom <bgonzalez130@ucmerced.edu>
2025-11-18 03:50:04 -0800

    Admin paga and fixed template

--- app.py
+++ app.py
@@ -23,8 +23,14 @@ login_manager = LoginManager()
 login_manager.init_app(application)
 login_manager.login_view = "login"
 
+# fixes  error
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 # --------------------------------------
 # Admin Panel (Flask-Admin)
+# --------------------------------------
 # --------------------------------------
 admin = Admin(application, name="ACME Admin")
 admin.add_view(ModelView(User, db.session))
@@ -32,15 +38,20 @@ admin.add_view(ModelView(Class, db.session))
 admin.add_view(ModelView(Enrollment, db.session))
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
 # --------------------------------------
 # LOGIN ROUTES
 # --------------------------------------
 @application.route("/", methods=["GET", "POST"])
 def login():
+     # if already logged in, don’t show login page —> sent to dashboard
+    if current_user.is_authenticated:
+        if current_user.role == "student":
+            return redirect("/student")
+        elif current_user.role == "teacher":
+            return redirect("/teacher")
+        elif current_user.role == "admin":
+            return redirect("/adminpage")
+        
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -81,8 +92,7 @@ def student_my_classes():
 @login_required
 def student_all_classes():
     classes = Class.query.all()
-    my_ids = {e.class_id for e in current_user.enrollments}
-    return render_template("student_all_classes.html", classes=classes, my_ids=my_ids)
+    return render_template("student_all_classes.html", classes=classes)
 
 
 @application.route("/student/enroll/<int:class_id>")
@@ -100,18 +110,8 @@ def enroll(class_id):
     enroll = Enrollment(student_id=current_user.id, class_id=class_id)
     db.session.add(enroll)
     db.session.commit()
-    return redirect("/student/all_classes")
+    return redirect("/student/my_classes")
 
-@application.route("/student/unenroll/<int:class_id>")
-@login_required
-def unenroll(class_id):
-    enrollment = Enrollment.query.filter_by(student_id=current_user.id, class_id=class_id).first()
-    if not enrollment:
-        return "Not Enrolled in this class!"
-
-    db.session.delete(enrollment)
-    db.session.commit()
-    return redirect("/student/all_classes")
 
 # --------------------------------------
 # TEACHER ROUTES
@@ -119,8 +119,6 @@ def unenroll(class_id):
 @application.route("/teacher")
 @login_required
 def teacher_dashboard():
-    if current_user.role != "teacher":
-        return redirect("/")
     classes = current_user.classes
     return render_template("teacher_dashboard.html", classes=classes)
 
@@ -128,26 +126,15 @@ def teacher_dashboard():
 @application.route("/teacher/class/<int:class_id>", methods=["GET", "POST"])
 @login_required
 def teacher_class(class_id):
-    if current_user.role != "teacher":
-        return redirect("/")
-
     cls = Class.query.get(class_id)
-
-    # prevent teachers from accessing other teachers' classes
-    if cls.teacher_id != current_user.id:
-        return "Unauthorized", 403
-
     enrollments = cls.enrollments
 
     if request.method == "POST":
         eid = request.form["eid"]
         grade = request.form["grade"]
-
         enr = Enrollment.query.get(eid)
         enr.grade = grade
         db.session.commit()
-
-        return redirect(f"/teacher/class/{class_id}")  # refresh after save
 
     return render_template("teacher_class.html", cls=cls, enrollments=enrollments)
 
@@ -158,8 +145,21 @@ def teacher_class(class_id):
 @application.route("/adminpage")
 @login_required
 def admin_page():
-    return render_template("admin.html")
+    if current_user.role != "admin":
+        return redirect("/")
 
+    # query data for the dashboard
+    users = User.query.all()
+    classes = Class.query.all()
+    enrollments = Enrollment.query.all()
+
+    # pass to template
+    return render_template(
+        "admin.html",
+        users=users,
+        classes=classes,
+        enrollments=enrollments,
+    )
 
 # --------------------------------------
 # Run Server
@@ -172,8 +172,9 @@ if __name__ == "__main__":
 # 1. pip install -r requirements.txt (this installs all necessary flask packages)
 # 2. run python seed.py
 # 3. run python app.py
-# logins:
-# admin/admin
-# mindy/123
-# chuck/123
-# ahepworth/123
+
+# these are the logins that work:
+# admin (username)/ admin (password)
+# mindy (username) / 123 (password)
+#c huck (username) / 123 (password)
+# ahepworth (username) / 123 (password)
